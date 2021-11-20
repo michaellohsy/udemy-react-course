4113b6ef85f65bed72a69d83593295d46d677798
Mike <michaellohsy@gmail.com>
2021-11-20 16:46:32 +0800

    Added list key

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,6 +19,7 @@ function Expenses(props) {
         <ExpensesFilter year={selectedYear} onYearChange={onYearChange} />
         {expenses.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
